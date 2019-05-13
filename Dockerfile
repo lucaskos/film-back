a5950ce9e18bc924cec5a18de44ff3c6a88359aa
lkosmala <kosmala.luke@gmail.com>
2019-05-14 00:23:25 +0200

    Dockerizing

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM openjdk:8-jdk-alpine
-VOLUME /tmp
-
-COPY target/app.jar app.jar
-EXPOSE 8083
-
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- app/src/main/java/com/example/demo/application/repository/PersonRepo.java
+++ app/src/main/java/com/example/demo/application/repository/PersonRepo.java
@@ -3,11 +3,12 @@ package com.example.demo.application.repository;
 import com.example.demo.application.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PersonRepo extends JpaRepository<Person, Long> {
 
     @Query("select p from Person p where p.firstName LIKE CONCAT('%', :name, '%') OR p.lastName LIKE CONCAT('%', :name, '%')")
-    List<Person> autocompleteByFirstNameOrLastName(String name);
+    List<Person> autocompleteByFirstNameOrLastName(@Param("name") String name);
 }
